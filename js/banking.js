072e953e4fb8c3bc3f1ea26eeac085dc4c44045c
toufiq007 <t.limon2342@gmail.com>
2021-08-15 21:35:20 +0600

    deposite money logic

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,13 @@
 // making banking page functional
+// deposite money logic
+document.getElementById("depositeButton").addEventListener("click",function(){
+  let depositeInput = document.getElementById("depositeInput");
+  let previousDepositeAmmount = document.getElementById("previousDepositeAmmount");
+  let previousMainAmmount = document.getElementById("previousMainAmmount");
+
+  // calculate deposite ammount
+  previousDepositeAmmount.innerText = parseFloat(previousDepositeAmmount.innerText) + parseFloat(depositeInput.value)
+  // calculate main balance
+  previousMainAmmount.innerText = parseFloat(previousMainAmmount.innerText) - parseFloat(depositeInput.value);
+  depositeInput.value = ""
+})
