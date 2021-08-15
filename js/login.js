092f5cc32ec2515ce20b7c97c7b172ed246b8462
toufiq007 <t.limon2342@gmail.com>
2021-08-15 20:48:08 +0600

    created login page and redirect this

---
+++ js/login.js
@@ -0,0 +1,8 @@
+// redirect page
+document.getElementById("submit-button").addEventListener("click",function(){
+  let emailValue = document.getElementById("userEmail");
+  let passValue  = document.getElementById("userPass");
+  if (emailValue.value == "limon@gmail.com" && passValue.value == "limon23"){
+    window.location.href = "banking.html";
+  }
+})
