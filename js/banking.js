5281457c2c8caa2ae3558ee48082f3394048cd3e
toufiq007 <t.limon2342@gmail.com>
2021-08-15 21:44:16 +0600

    widthdraw logic

--- js/banking.js
+++ js/banking.js
@@ -8,6 +8,22 @@ document.getElementById("depositeButton").addEventListener("click",function(){
   // calculate deposite ammount
   previousDepositeAmmount.innerText = parseFloat(previousDepositeAmmount.innerText) + parseFloat(depositeInput.value)
   // calculate main balance
-  previousMainAmmount.innerText = parseFloat(previousMainAmmount.innerText) - parseFloat(depositeInput.value);
+  previousMainAmmount.innerText = parseFloat(previousMainAmmount.innerText) + parseFloat(depositeInput.value);
   depositeInput.value = ""
+})
+
+
+// widthdraw money logic
+document.getElementById("widthdrawButton").addEventListener("click",function(){
+  let widthDrawInput = document.getElementById("widthDrawInput");
+  let previouswidthdrawAmmount = document.getElementById("previouswidthdrawAmmount");
+  let previousMainAmmount = document.getElementById("previousMainAmmount");
+
+  // calculate widthdraw ammount
+  previouswidthdrawAmmount.innerText = parseFloat(previouswidthdrawAmmount.innerText) + parseFloat(widthDrawInput.value);
+  
+
+  // calculate main balance
+  previousMainAmmount.innerText = parseFloat(previousMainAmmount.innerText) - parseFloat(widthDrawInput.value);
+  widthDrawInput.value = ""
 })
